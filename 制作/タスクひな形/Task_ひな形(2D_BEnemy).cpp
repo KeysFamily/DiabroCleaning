b3afa788a5b5b605e0cefd3a30a95aa7ded610be
keysfamily <pinhousehold.british@gmail.com>
2023-11-08 12:24:39 +0900

    [Add]BEnemyクラス、およびタスクひな形の追加

---
+++ 制作/タスクひな形/Task_ひな形(2D_BEnemy).cpp
@@ -0,0 +1,166 @@
+//?------------------------------------------------------
+//タスク名:
+//作　成　者:
+//TODO:もしいれば下記へ記述
+//編　集　者:
+//作成年月日:
+//概　　　要:
+//?------------------------------------------------------
+#include  "MyPG.h"
+#include  "該当する.h"
+
+namespace  「ネームスペース名」
+{
+	Resource::WP  Resource::instance;
+	//-------------------------------------------------------------------
+	//リソースの初期化
+	bool  Resource::Initialize()
+	{
+		return true;
+	}
+	//-------------------------------------------------------------------
+	//リソースの解放
+	bool  Resource::Finalize()
+	{
+		return true;
+	}
+	//-------------------------------------------------------------------
+	//「初期化」タスク生成時に１回だけ行う処理
+	bool  Object::Initialize()
+	{
+		//スーパークラス初期化
+		__super::Initialize(defGroupName, defName, true);
+		//リソースクラス生成orリソース共有
+		this->res = Resource::Create();
+
+		//★データ初期化
+		
+		//★タスクの生成
+
+		return  true;
+	}
+	//-------------------------------------------------------------------
+	//「終了」タスク消滅時に１回だけ行う処理
+	bool  Object::Finalize()
+	{
+		//★データ＆タスク解放
+
+
+		if (!ge->QuitFlag() && this->nextTaskCreate) {
+			//★引き継ぎタスクの生成
+		}
+
+		return  true;
+	}
+	//-------------------------------------------------------------------
+	//「更新」１フレーム毎に行う処理
+	void  Object::UpDate()
+	{
+		this->UpDate_Std();
+		//当たり判定仮処理
+		//BChara::AttackInfo at = {1,0,0};
+		// if(this->Attack_Std<BChara>(Player::defGroupName,at)){
+		// 	//接触した際、自身に何かをする際の処理
+		// }
+	}
+	//-------------------------------------------------------------------
+	//「２Ｄ描画」１フレーム毎に行う処理
+	void  Object::Render2D_AF()
+	{
+		//this->Render_Std(this->res->img);
+		
+	}
+	
+	//-------------------------------------------------------------------
+	// 思考制御
+	void Object::Think(){
+		
+	}
+	
+	//-------------------------------------------------------------------
+	// 動作制御
+	void Object::Move(){
+		
+	}
+	
+	//-------------------------------------------------------------------
+	// アニメーション制御
+	BEnemy::DrawInfo Object::Anim(){
+		
+	}
+	
+	void Object::Received(BEnemy* from_, AttackInfo at_) {
+		if (this->unHitTime > 0) { 
+			return; //無敵時間中は処理を受けない
+		}
+		this->unHitTime = 30;
+		this->hp -= at_.power;
+		if (this->hp <= 0) {
+			this->Kill();
+		}
+		//吹き飛ばされる
+		if (this->pos.x > from_->pos.x) {
+			this->moveVec = ML::Vec2(+3, -8);
+		}
+		else {
+			this->moveVec = ML::Vec2(-3, -8);
+		}
+		this->UpdateMotion(Motion::Bound);
+	}
+
+	//★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+	//以下は基本的に変更不要なメソッド
+	//★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+	//-------------------------------------------------------------------
+	//タスク生成窓口
+	Object::SP  Object::Create(bool  flagGameEnginePushBack_)
+	{
+		Object::SP  ob = Object::SP(new  Object());
+		if (ob) {
+			ob->me = ob;
+			if (flagGameEnginePushBack_) {
+				ge->PushBack(ob);//ゲームエンジンに登録
+				
+			}
+			if (!ob->B_Initialize()) {
+				ob->Kill();//イニシャライズに失敗したらKill
+			}
+			return  ob;
+		}
+		return nullptr;
+	}
+	//-------------------------------------------------------------------
+	bool  Object::B_Initialize()
+	{
+		return  this->Initialize();
+	}
+	//-------------------------------------------------------------------
+	Object::~Object() { this->B_Finalize(); }
+	bool  Object::B_Finalize()
+	{
+		auto  rtv = this->Finalize();
+		return  rtv;
+	}
+	//-------------------------------------------------------------------
+	Object::Object() {	}
+	//-------------------------------------------------------------------
+	//リソースクラスの生成
+	Resource::SP  Resource::Create()
+	{
+		if (auto sp = instance.lock()) {
+			return sp;
+		}
+		else {
+			sp = Resource::SP(new  Resource());
+			if (sp) {
+				sp->Initialize();
+				instance = sp;
+			}
+			return sp;
+		}
+	}
+	//-------------------------------------------------------------------
+	Resource::Resource() {}
+	//-------------------------------------------------------------------
+	Resource::~Resource() { this->Finalize(); }
+}
