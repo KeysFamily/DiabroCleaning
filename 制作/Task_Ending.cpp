5482a7b96fa1c9313537ca796beb82c9c03c5622
KeysFamily <pinhousehold.british@gmail.com>
2023-11-09 13:21:19 +0900

    [Add,Edit]タイトル、ゲーム、エンディングタスクの追加。それに伴うMyPGの操作。エフェクトがカメラに追従するかどうかの変数の追加、vcxproj.filtersの修正

---
+++ 制作/Task_Ending.cpp
@@ -0,0 +1,128 @@
+//-------------------------------------------------------------------
+//エンディング
+//-------------------------------------------------------------------
+#include  "MyPG.h"
+#include  "Task_Ending.h"
+#include  "Task_Title.h"
+
+namespace  Ending
+{
+	Resource::WP  Resource::instance;
+	//-------------------------------------------------------------------
+	//リソースの初期化
+	bool  Resource::Initialize()
+	{
+		return true;
+	}
+	//-------------------------------------------------------------------
+	//リソースの解放
+	bool  Resource::Finalize()
+	{
+		return true;
+	}
+	//-------------------------------------------------------------------
+	//「初期化」タスク生成時に１回だけ行う処理
+	bool  Object::Initialize()
+	{
+		//スーパークラス初期化
+		__super::Initialize(defGroupName, defName, true);
+		//リソースクラス生成orリソース共有
+		this->res = Resource::Create();
+
+		//★データ初期化
+
+		//★タスクの生成
+
+		return  true;
+	}
+	//-------------------------------------------------------------------
+	//「終了」タスク消滅時に１回だけ行う処理
+	bool  Object::Finalize()
+	{
+		//★データ＆タスク解放
+
+
+		if (!ge->QuitFlag() && this->nextTaskCreate) {
+			//★引き継ぎタスクの生成
+			auto  nextTask = Title::Object::Create(true);
+		}
+
+		return  true;
+	}
+	//-------------------------------------------------------------------
+	//「更新」１フレーム毎に行う処理
+	void  Object::UpDate()
+	{
+		auto inp = ge->in1->GetState();
+
+		if (inp.ST.down && ge->getCounterFlag("End") != ge->ACTIVE) {
+			ge->StartCounter("End", 45); //フェードは90フレームなので半分の45で切り替え
+			ge->CreateEffect(98, ML::Vec2(0, 0));
+
+		}
+		if (ge->getCounterFlag("End") == ge->LIMIT) {
+			this->Kill();
+		}
+	}
+	//-------------------------------------------------------------------
+	//「２Ｄ描画」１フレーム毎に行う処理
+	void  Object::Render2D_AF()
+	{
+		ge->Dbg_ToDisplay(100, 100, "Ending");
+	}
+
+	//★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+	//以下は基本的に変更不要なメソッド
+	//★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+	//-------------------------------------------------------------------
+	//タスク生成窓口
+	Object::SP  Object::Create(bool  flagGameEnginePushBack_)
+	{
+		Object::SP  ob = Object::SP(new  Object());
+		if (ob) {
+			ob->me = ob;
+			if (flagGameEnginePushBack_) {
+				ge->PushBack(ob);//ゲームエンジンに登録
+			}
+			if (!ob->B_Initialize()) {
+				ob->Kill();//イニシャライズに失敗したらKill
+			}
+			return  ob;
+		}
+		return nullptr;
+	}
+	//-------------------------------------------------------------------
+	bool  Object::B_Initialize()
+	{
+		return  this->Initialize();
+	}
+	//-------------------------------------------------------------------
+	Object::~Object() { this->B_Finalize(); }
+	bool  Object::B_Finalize()
+	{
+		auto  rtv = this->Finalize();
+		return  rtv;
+	}
+	//-------------------------------------------------------------------
+	Object::Object() {	}
+	//-------------------------------------------------------------------
+	//リソースクラスの生成
+	Resource::SP  Resource::Create()
+	{
+		if (auto sp = instance.lock()) {
+			return sp;
+		}
+		else {
+			sp = Resource::SP(new  Resource());
+			if (sp) {
+				sp->Initialize();
+				instance = sp;
+			}
+			return sp;
+		}
+	}
+	//-------------------------------------------------------------------
+	Resource::Resource() {}
+	//-------------------------------------------------------------------
+	Resource::~Resource() { this->Finalize(); }
+}
---
+++ 制作/Task_Title.cpp
@@ -0,0 +1,133 @@
+//-------------------------------------------------------------------
+//タイトル画面
+//-------------------------------------------------------------------
+#include  "MyPG.h"
+#include  "Task_Title.h"
+
+#include  "Task_Effect00.h"
+
+#include "Task_Game.h"
+
+namespace  Title
+{
+	Resource::WP  Resource::instance;
+	//-------------------------------------------------------------------
+	//リソースの初期化
+	bool  Resource::Initialize()
+	{
+		return true;
+	}
+	//-------------------------------------------------------------------
+	//リソースの解放
+	bool  Resource::Finalize()
+	{
+		return true;
+	}
+	//-------------------------------------------------------------------
+	//「初期化」タスク生成時に１回だけ行う処理
+	bool  Object::Initialize()
+	{
+		//スーパークラス初期化
+		__super::Initialize(defGroupName, defName, true);
+		//リソースクラス生成orリソース共有
+		this->res = Resource::Create();
+
+		//★データ初期化
+
+		//★タスクの生成
+		return  true;
+	}
+	//-------------------------------------------------------------------
+	//「終了」タスク消滅時に１回だけ行う処理
+	bool  Object::Finalize()
+	{
+		//★データ＆タスク解放
+		//bgm::Stop("bgm1");
+		if (!ge->QuitFlag() && this->nextTaskCreate) {
+			Game::Object::Create(true);
+		}
+
+		return  true;
+	}
+	//-------------------------------------------------------------------
+	//「更新」１フレーム毎に行う処理
+	void  Object::UpDate()
+	{
+		auto inp = ge->in1->GetState();
+
+		if (inp.ST.down && ge->getCounterFlag("title") != ge->ACTIVE) {
+			ge->StartCounter("title", 45); //フェードは90フレームなので半分の45で切り替え
+			ge->CreateEffect(98, ML::Vec2(0, 0));
+
+		}
+		if (ge->getCounterFlag("title") == ge->LIMIT) {
+			this->Kill();
+		}
+
+
+		return;
+
+	}
+	//-------------------------------------------------------------------
+	//「２Ｄ描画」１フレーム毎に行う処理
+	void  Object::Render2D_AF()
+	{
+		ge->Dbg_ToDisplay(100, 100, "Title");
+	}
+	
+
+	//★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+	//以下は基本的に変更不要なメソッド
+	//★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+	//-------------------------------------------------------------------
+	//タスク生成窓口
+	Object::SP  Object::Create(bool  flagGameEnginePushBack_)
+	{
+		Object::SP  ob = Object::SP(new  Object());
+		if (ob) {
+			ob->me = ob;
+			if (flagGameEnginePushBack_) {
+				ge->PushBack(ob);//ゲームエンジンに登録
+			}
+			if (!ob->B_Initialize()) {
+				ob->Kill();//イニシャライズに失敗したらKill
+			}
+			return  ob;
+		}
+		return nullptr;
+	}
+	//-------------------------------------------------------------------
+	bool  Object::B_Initialize()
+	{
+		return  this->Initialize();
+	}
+	//-------------------------------------------------------------------
+	Object::~Object() { this->B_Finalize(); }
+	bool  Object::B_Finalize()
+	{
+		auto  rtv = this->Finalize();
+		return  rtv;
+	}
+	//-------------------------------------------------------------------
+	Object::Object() {	}
+	//-------------------------------------------------------------------
+	//リソースクラスの生成
+	Resource::SP  Resource::Create()
+	{
+		if (auto sp = instance.lock()) {
+			return sp;
+		}
+		else {
+			sp = Resource::SP(new  Resource());
+			if (sp) {
+				sp->Initialize();
+				instance = sp;
+			}
+			return sp;
+		}
+	}
+	//-------------------------------------------------------------------
+	Resource::Resource() {}
+	//-------------------------------------------------------------------
+	Resource::~Resource() { this->Finalize(); }
+}
